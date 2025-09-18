75932ff5f789d47d528e4a4cc25375838679535b
Ifeanyi Njoku <injoku@my.centennialcollege.ca>
2025-09-17 21:16:38 -0400

    Completed Projects and Services page. Added a bit of css to them. Also places css files in their own folder

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,7 +1,26 @@
 export default function About() {
   return (
-    <>
+    <div className="about-me-container">
+      {/* Page Title */}
       <h1>About Me</h1>
-    </>
+
+      {/* Profile Image (Will be added Later) */}
+
+      {/* About me Paragraph */}
+      <p>
+        I am a Software Engineering student, currently attending Centennial College, that 
+        has a passion for building creative and meaningful applications. My interests include web
+        development, DevOps, game development and solving real-world problems through code.
+      </p>
+
+      {/* Resume Download */}
+      <a
+        href="/src/assets/ResumePDF/Ifeanyi-Njoku-2.pdf"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        View My Resume
+      </a>
+    </div>
   );
 }
